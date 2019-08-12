63dc32ef10bba03e838318cdf1278624af36545b
julio <julio.rodriguezr1@gmail.com>
2019-08-12 07:46:11 -0300

    12 aug 2019

--- js/function.js
+++ js/function.js
@@ -1,69 +1,67 @@
 var app = angular.module("evalApp", ["ngRoute"])
-	
-		app.config(function($routeProvider) {
-			$routeProvider
-			.when("/", {
-				templateUrl : "content.html",
-				controller: "contentController"				
-			})
-			.when("/about", {
-				templateUrl : "sobremi.html",
-				controller: "sobremiController"	
-			})
 
-		});
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "content.html",
+            controller: "contentController"
+        })
+        .when("/about", {
+            templateUrl: "sobremi.html",
+            controller: "sobremiController"
+        })
 
-		app.controller('contentController', function($scope) {
-			$scope.message = 'Exito cargando el html principal';
-		  });
+});
 
-		  app.controller('sobremiController', function($scope) {	
-			$scope.message = 'Exito cargando el About';
-			$( "#accordion" ).accordion();
-		  });	
+app.controller('contentController', function($scope) {
+    $scope.message = 'AngularJS: Exito cargando el html principal';
+});
 
-
-$(document).ready(function () {		  
-
-		date = new Date();
-
-		var d = moment(date).format("dddd,MMMM Do YYYY")
-		var startdate = moment().subtract(1, "days").format("dddd,MMMM Do YYYY");
-
-		$(".postDate").html(d) // declarative with AngularJS -- Apply controller.
-		$(".postDate2").html(startdate) // declarative with AngularJS -- Apply controller.
-
-		$('.bxslider').bxSlider({
-		  auto: true,
-		  autoControls: true,
-		  stopAutoOnClick: true,
-		  pager: true,
-		  responsive: true,
-		});
+app.controller('sobremiController', function($scope) {
+    $scope.message = 'AngularJS: Exito cargando el About';
+    $("#accordion").accordion();
+});
 
 
-	$("#change-theme div").click(function () {
-	
-		if(this.id == "to-green"){
-			$("#themes").attr("href","css/green.css")
-		}
-		else if(this.id == "to-blue"){
-			 $("#themes").attr("href","css/blue.css")
-		}
-		else if(this.id == "to-red"){
-			 $("#themes").attr("href","css/red.css")
-		}
+$(document).ready(function() {
 
-	})
+    date = new Date();
 
-	$(".goUp").click(function (e) {
-		e.preventDefault();
+    var d = moment(date).format("dddd,MMMM Do YYYY")
+    var startdate = moment().subtract(1, "days").format("dddd,MMMM Do YYYY");
 
-		$("html,body").animate({
-			scrollTop: 0
-		},500)
-	})
+    $(".postDate").html(d) // declarative with AngularJS -- Apply controller.
+    $(".postDate2").html(startdate) // declarative with AngularJS -- Apply controller.
 
-$(".modais").iziModal({ history: false,  fullscreen: true, loop: true, zindex: 99999, focusInput: true });
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        responsive: true,
+    });
+
+
+    $("#change-theme div").click(function() {
+
+        if (this.id == "to-green") {
+            $("#themes").attr("href", "css/green.css")
+        } else if (this.id == "to-blue") {
+            $("#themes").attr("href", "css/blue.css")
+        } else if (this.id == "to-red") {
+            $("#themes").attr("href", "css/red.css")
+        }
+
+    })
+
+    $(".goUp").click(function(e) {
+        e.preventDefault();
+
+        $("html,body").animate({
+            scrollTop: 0
+        }, 500)
+    })
+
+    $(".modais").iziModal({ history: false, fullscreen: true, loop: true, zindex: 99999, focusInput: true });
 
 })
